0489cac687f749ce63d484c46a0dc49d6662076a
Anna A <anicka_88@mail.ru>
2022-07-12 14:00:00 +0300

    new file

---
+++ extentions.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class ForexConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}')
+
+        if amount == float(amount):
+            raise APIException(f'Не удалось обработать сумму {amount}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]] * int(amount)
+
+        return total_base
